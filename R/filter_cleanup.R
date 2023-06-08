8d05bca0b6a32c26b824d0f1eba2c97d662d134b
Andrew Mack <65462867+armack@users.noreply.github.com>
2023-06-08 17:49:29 -0400

    Improve varaible names and documentation

--- R/filter_cleanup.R
+++ R/filter_cleanup.R
@@ -13,20 +13,30 @@ NULL
 #'   Additionally, `filter_microbigge()` will drop rows not meeting minimum
 #'   thresholds of `coverage` and `identity`.
 #'
-#'   ## Allele calling method The `method` column contains codes representing
-#'   the calling method used for a given allele, some of which correspond to
-#'   "less than perfect" matches and may warrant removal depending on the scope
-#'   and goals of the analysis. Detailed explanations are available at:
-#'   [https://www.ncbi.nlm.nih.gov/pathogens/pathogens_help/#genotype-categories]
+#'   ## Allele calling method
+#'   The `method` column contains codes representing the calling method used for
+#'   a given allele, some of which correspond to "less than perfect" matches and
+#'   may warrant removal depending on the scope and goals of the analysis.
 #'
-#'   ## Coverage and identity These values are produced by AMRFinderPlus as part
-#'   of the analysis pipeline:
+#'   More detailed explinations of these calling methods and the specific
+#'   parameters used to generate them is available from NCBI at
+#'   [https://www.ncbi.nlm.nih.gov/pathogens/pathogens_help/#genotype-categories]
+#'   and [https://github.com/ncbi/amr/wiki/Methods]
+#'
+#'   ## Coverage and identity
+#'   These values are produced by AMRFinderPlus as part of the analysis pipeline
+#'   and percentages use the closest sequence in the the Reference Gene Catalog
+#'   used as a reference.
+#'
 #'   * `coverage` is the percentage of the reference sequence covered by the alignment between the target and the reference sequence
 #'   * `identity` is the percentage of identical amino acids or base pairs in the alignment between the target and reference sequence
 #'
-#'   The default thresholds correspond to those used in Isolates Browser (and
-#'   thus `amr.metadata.tsv` files on the NCBI FTP server). See more info at:
+#'   The default `identity` threshold corresponds to the AMRFinderPlus default of >=90% for BLAST matches. (and
+#'   thus `amr.metadata.tsv` files on the NCBI FTP server).
+#'
+#'   More information is available from NCBI at
 #'   [https://www.ncbi.nlm.nih.gov/pathogens/pathogens_help/#genotype-categories]
+#'   and [https://github.com/ncbi/amr/wiki/Methods]
 #'
 #'   ## Deduplication
 #'   Removes duplicate `allele`/`biosample` combinations, which can be included
@@ -43,8 +53,8 @@ NULL
 #' @returns `data` with rows removed according to the selected parameters
 #' @export
 filter_isolates_browser <- function(data, filter, remove, deduplicate = TRUE) {
-  has_filter <- missing(filter)
-  has_remove <- missing(remove)
+  has_filter <- !missing(filter)
+  has_remove <- !missing(remove)
 
   .complete <- data %>%
     {if(has_filter) dplyr::filter(., grepl(paste(filter, collapse = "|"), .data$allele)) else . } %>%
@@ -61,15 +71,15 @@ filter_isolates_browser <- function(data, filter, remove, deduplicate = TRUE) {
 filter_microbigge <- function(data, coverage = 100L, identity = 90L, filter, remove, deduplicate = TRUE) {
   . <- NULL # Workaround to suppress `no visible binding for global variable`
 
-  has_filter <- missing(filter)
-  has_remove <- missing(remove)
+  has_filter <- !missing(filter)
+  has_remove <- !missing(remove)
 
   .complete <- data %>%
     dplyr::filter("coverage" >= coverage) %>%
-    dplyr::filter("identity" >= identity)
+    dplyr::filter("identity" >= identity) %>%
     {if(has_filter) dplyr::filter(., grepl(paste(filter, collapse = "|"), .data$allele)) else . } %>%
     {if(has_remove) dplyr::filter(., !grepl(paste(remove, collapse = "|"), .data$method)) else .} %>%
-    {if(deduplicate) dplyr::arrange(., .data$protein) %>% dplyr::distinct(.data$biosample, .data$allele, .keep_all = TRUE) else . }
+    {if(deduplicate) dplyr::distinct(dplyr::arrange(., .data$protein), .data$biosample, .data$allele, .data$ipg, .keep_all = TRUE) else . }
 
   return(.complete)
 }
